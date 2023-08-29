02dc3c739f8e20695bc14969efe0bf39fa812a02
yunjeong <stac_cato@naver.com>
2023-08-29 19:51:28 +0900

    기본 설정 완료

---
+++ spring-bappo/src/main/java/bappo/springbappo/SpringBappoApplication.java
@@ -0,0 +1,13 @@
+package bappo.springbappo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBappoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBappoApplication.class, args);
+	}
+
+}
---
+++ spring-bappo/src/test/java/bappo/springbappo/SpringBappoApplicationTests.java
@@ -0,0 +1,13 @@
+package bappo.springbappo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBappoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
